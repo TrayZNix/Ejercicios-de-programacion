a8c8a00bea16b573c2aa47d027b62cca44ba9394
Roberto Carvajal Velasco <carvajal.verob22@triana.salesianos.edu>
2021-10-26 09:58:38 +0200

    Archivo hola mundo actualizado

--- Hola_Mundo/src/paquete/Principal.java
+++ Hola_Mundo/src/paquete/Principal.java
@@ -16,7 +16,7 @@ public class Principal {
 		System.out.println(dia);
 		System.out.println();
 		System.out.println("Precio actual de BTC: " +bitcoin+"$"+"\n");
-
+		System.out.println("ESTE TEXTO CONFIRMA QUE EL ARCHIVO SE HA ACTUALIZADO GRACIAS A GIT");
 		System.out.printf("Buenos días señor %s %s.\n", nombre01 , apellido01);
 		System.out.printf("Su sueldo es: %.2f€, y su extra por comisiones es %.2f€\n", sueldo2, sueldo);
 		if (continuo == 1) {
