d6f8e754a0b693d45dd177406f1660b3277af5a5
Roberto Carvajal <robertocarvajalvelasco@gmail.com>
2021-11-08 10:51:58 +0100

    Comienzo de ejemplos de objetos

--- EjemplosTema3/src/ejemplo01/Alumno.java
+++ EjemplosTema3/src/ejemplo01/Alumno.java
@@ -1,5 +1,18 @@
 package ejemplo01;
 
 public class Alumno {
-
+	//Atributos
+	
+	int edad;
+	String nombre;
+	String apellidos;
+	double notaMedia;
+	
+	//Constructor
+	public Alumno (int edad, String nombre, String apellidos, double notaMedia) {
+		this.edad=edad;
+		this.nombre=nombre;
+		this.apellidos=apellidos;
+		this.notaMedia=notaMedia;
+	}
 }
--- EjemplosTema3/src/ejemplo01/Principal.java
+++ EjemplosTema3/src/ejemplo01/Principal.java
@@ -4,7 +4,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Alumno a1= new Alumno (18, "Roberto", "Carvajal", 9.2);
+		System.out.println(a1);
 	}
 
 }
