5232b7f5c2c6da8063b2432f8f7273e91b3eb9e0
Roberto Carvajal <robertocarvajalvelasco@gmail.com>
2021-11-09 14:25:36 +0100

    w

--- EjemplosTema3/src/ejemplo01/Principal.java
+++ EjemplosTema3/src/ejemplo01/Principal.java
@@ -1,10 +1,23 @@
 package ejemplo01;
 
+import lectura.leer;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Alumno a1= new Alumno (18, "Roberto", "Carvajal", 9.2);
+		int a=0; 
+		double b=0;
+		String sa, sb;
+		System.out.printf("Introduzca su edad:\n");
+		a=leer.datoInt();
+		System.out.printf("Introduzca su nombre:\n");
+		sa=leer.dato();
+		System.out.printf("Introduzca su/sus apellido/s:\n");
+		sb=leer.dato();
+		System.out.printf("Introduzca su nota media:\n");
+		b=leer.datoDouble();
+		Alumno a1= new Alumno (a, sa, sb, b);
 		System.out.println(a1);
 	}
 
