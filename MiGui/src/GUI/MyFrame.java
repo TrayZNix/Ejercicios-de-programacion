55c506c38573b52c2f3c6c9c1a1accd3c134efcb
Roberto Carvajal <robertocarvajalvelasco@gmail.com>
2022-02-07 00:42:07 +0100

    Comenzada GUIlearning

---
+++ MiGui/src/GUI/MyFrame.java
@@ -0,0 +1,17 @@
+package GUI;
+
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+	
+	MyFrame(){
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	this.setLayout(null);
+	this.setSize(750, 750);
+	this.setVisible(true);
+	}
+}
+
