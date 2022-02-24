e7ead755b5457528a8009991f067bf97d1e36b1d
Roberto Carvajal <robertocarvajalvelasco@gmail.com>
2022-02-24 10:54:38 +0100

    .

---
+++ BoletinUD5/src/ejercicio01/Agenda.java
@@ -0,0 +1,14 @@
+package ejercicio01;
+
+import java.util.List;
+/**
+ * Esta clase es un que conforma una agenda, la cual contiene notas.
+ * Posee un ArrayList de notas.
+ * @author carvajal.verob22
+ * @version 1.0 23/02/2022
+ */
+public class Agenda {
+	private int idAgenda;
+	private List <Nota> lista;
+	
+}
