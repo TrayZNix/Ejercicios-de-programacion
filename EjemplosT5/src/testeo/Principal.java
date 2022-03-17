ba5cbfb37461b42684b122ab023d897d0f665575
Roberto Carvajal <robertocarvajalvelasco@gmail.com>
2022-03-17 10:54:02 +0100

    .

--- EjemplosT5/src/testeo/Principal.java
+++ EjemplosT5/src/testeo/Principal.java
@@ -1,12 +1,17 @@
 package testeo;
 
+import java.util.stream.DoubleStream;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		FactoryFiguras factoriaFiguras = new FactoryFiguras();
+
+		System.out.println(factoriaFiguras.getFigura("Rectangulo").toString());
 		
-		System.out.println(factoriaFiguras.getFigura("Cuadrado").toString());
+		
+		
+		System.out.println();
 	}
 
 }
